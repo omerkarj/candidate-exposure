814cc5b16d6bb75c566e9cd37a03982eea1d29f4
omerkarj <omerkarj@gmail.com>
2016-01-03 13:59:40 +0200

    express-project

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,30 @@
+var app = angular.module('candidateExposure', []);
+
+app.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', [ '$scope', 'posts',
+    function ($scope, posts) {
+        $scope.test = 'Hello world!';
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+    }
+]);
